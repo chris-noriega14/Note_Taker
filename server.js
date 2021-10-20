6d999f19966a8fda7bef53e75b1bc94e175db0f5
chris-noriega14 <chris.noriega14@hotmail.com>
2021-10-20 15:44:30 -0500

    Added a routing folder with two route files,local host filepath linked to index.html and notes.html

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const viewRoutes = require('/routing/view-routes.js');
 
 const PORT = 3001;
 const app = express();
@@ -7,5 +8,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+app.use("/",viewRoutes);
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
